d064e8ea2f23864ae133d2eb644d59a344a2e44b
TitouanML <titouanmasselaude@gmail.com>
2026-03-15 16:59:23 +0100

    :wrench: Fixed wrong constants

--- app/services/api/apiClient.ts
+++ app/services/api/apiClient.ts
@@ -1,8 +1,8 @@
 import Constants from 'expo-constants';
 import secureStoreService from '../secureStore.service';
 
-const API_BASE_URL = Constants.expoConfig?.extra?.apiBaseUrl || 'http://localhost:5027/api';
-const API_KEY = Constants.expoConfig?.extra?.apiKey || 'e93a27d4-e39a-44b3-9ad1-58a43d75864d';
+const API_BASE_URL = Constants.expoConfig?.extra?.apiBaseUrl || 'http://localhost:3000';
+const API_KEY = Constants.expoConfig?.extra?.apiKey || 'API_KEY';
 
 export class ApiError extends Error {
   constructor(
